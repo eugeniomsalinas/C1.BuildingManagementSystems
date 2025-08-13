bc1442c4f837bc4fa17355e9a3fac79a5ced4712
Eugenio Salinas <eugeniomsalinas@gmail.com>
2025-08-13 15:56:47 -0500

    Add project files.

---
+++ C1.BuildingManagementSystems.Contracts/IBuildingMetricsService.cs
@@ -0,0 +1,9 @@
+﻿namespace C1.BuildingManagementSystems.Contracts
+{
+    public interface IBuildingMetricsService
+    {
+        Task<string> Get(string request);
+
+        Task<string> Push(string request);
+    }
+}
---
+++ C1.BuildingManagementSystems/api/metrics/BuildingManagementSystemsService.cs
@@ -0,0 +1,29 @@
+﻿using C1.BuildingManagementSystems.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace C1.BuildingManagementSystems.api.metrics
+{
+    [Route("api/metrics/[controller]")]
+    [ApiController]
+    public class BuildingMetricsService : ControllerBase, IBuildingMetricsService
+    {
+        public BuildingMetricsService()
+        {
+
+        }
+
+        [HttpGet]
+        [Route("GetMetrics")]
+        public async Task<string> Get(string request)
+        {
+            return "Hello World";
+        }
+
+        [HttpPost]
+        [Route("PushMetric")]
+        public async Task<string> Push(string request)
+        {
+            return "Hello World";
+        }
+    }
+}
